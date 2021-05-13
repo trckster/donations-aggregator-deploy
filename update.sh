39825562e6b5dea79877806ea08aaf594e7111b1
root <root@localhost.localdomain>
2021-05-13 17:46:20 +0300

    Add update script

---
+++ update.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+update_backend() {
+	echo "Updating backend"
+
+	cd /var/www/backend
+	git pull
+	cd /var/www/deploy
+	docker-compose up -d --no-deps --build backend
+	docker-compose exec backend php artisan migrate --force
+}
+
+update_frontend() {
+	echo "Updating frontend"
+	echo "No scripts for frontend yet."
+}
+
+updated_all() {
+	echo "Updating all"
+	update_backend
+	update_frontend
+}
+
+if [[ $1 = "backend" ]]; then
+	update_backend
+elif [[ $1 = "frontend" ]]; then
+	update_frontend
+else
+	updated_all
+fi
